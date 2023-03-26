2588195c9ee4f82c12e26476be63b724093c8006
Abe Chen <abe1272001@gmail.com>
2023-03-27 00:11:29 +0800

    feat: modal use absolute and ui will break by other absolute element

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <script setup lang="ts">
 import ModalConfirm from '@/components/ModalConfirm.vue'
+import { ref } from 'vue'
+
+const show = ref(false)
+
+const openConfirm = () => {
+  show.value = true
+}
+
+const closeConfirm = () => {
+  show.value = false
+}
 </script>
 
 <template>
-  <div>
-    <ModalConfirm />
+  <div class="flex justify-center items-center min-h-screen">
+    <button
+      class="border p-2 rounded-md hover:bg-blue-400 hover:text-white"
+      @click="openConfirm"
+    >
+      Open Confirm Modal
+    </button>
+    <ModalConfirm v-if="show" @close="closeConfirm" />
+    <div
+      class="absolute top-20 bg-red-600 w-full h-10 flex justify-center items-center text-white"
+    >
+      This will make ui ugly
+    </div>
   </div>
-  <div class="text-3xl font-bold underline">hi</div>
 </template>
--- src/components/ModalConfirm.vue
+++ src/components/ModalConfirm.vue
@@ -1,5 +1,30 @@
+<script setup lang="ts"></script>
+
 <template>
-  <div class="absolute inset-0 overflow-y-auto bg-black bg-opacity-50">
-    <div></div>
+  <div class="absolute inset-0 overflow-hidden bg-black bg-opacity-50">
+    <div class="flex items-start justify-center min-h-screen mt-24 text-center">
+      <div
+        class="bg-white text-black rounded-md text-center shadow-lg p-5 w-64"
+        role="dialog"
+        aria-modal="true"
+      >
+        <h3>Do you confirm?</h3>
+        <div class="flex justify-center py-4 text-white gap-4">
+          <!-- We will handle these emits later -->
+          <button
+            @click="$emit('close')"
+            class="border border-black bg-white text-black rounded-md w-16 py-2"
+          >
+            No
+          </button>
+          <button
+            @click="$emit('close')"
+            class="border border-black bg-black text-white rounded-md w-16 py-2"
+          >
+            Yes
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
