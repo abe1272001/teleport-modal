93ddd02d40706366e3e87d29b9fc34a65a4beb2c
Abe Chen <abe1272001@gmail.com>
2023-03-26 21:20:42 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import ModalConfirm from '@/components/ModalConfirm.vue'
+</script>
+
+<template>
+  <div>
+    <ModalConfirm />
+  </div>
+  <div class="text-3xl font-bold underline">hi</div>
+</template>
---
+++ src/components/ModalConfirm.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="absolute inset-0 overflow-y-auto bg-black bg-opacity-50">
+    <div></div>
+  </div>
+</template>
